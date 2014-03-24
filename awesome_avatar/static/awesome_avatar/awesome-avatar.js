af01c266ae9cec47dc7d91a5175c6e9435631ed1
Антон Белущенко <dilar05@i.ua>
2014-03-24 15:33:48 +0200

    bugfix

--- awesome_avatar/static/awesome_avatar/awesome-avatar.js
+++ awesome_avatar/static/awesome_avatar/awesome-avatar.js
@@ -46,6 +46,32 @@ var j = jQuery.noConflict();
         j(selector + '-select-area').empty();
         j(selector + '-select-area').append('<img />');
     //    $(selector + '-select-area img').show();
+        var ctx = canvas.getContext("2d");
+        var img = j(selector + '-preview img').get(0)
+        ctx.drawImage(img, 0, 0);
+        var MAX_WIDTH = 150;
+        var MAX_HEIGHT = 150;
+        var width = img.width;
+        var height = img.height;
+
+        if (width > height) {
+          if (width > MAX_WIDTH) {
+            height *= MAX_WIDTH / width;
+            width = MAX_WIDTH;
+          }
+        } else {
+          if (height > MAX_HEIGHT) {
+            width *= MAX_HEIGHT / height;
+            height = MAX_HEIGHT;
+          }
+        }
+        canvas.width = width;
+        canvas.height = height;
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0, width, height);
+        var dataurl = canvas.toDataURL("image/png");
+        j(selector + '-preview img').attr('src', dataurl);
+        image_data = dataurl;
         j(selector + '-select-area img').attr('src', image_data).load(function(){
             j(this).unbind('load');
 
