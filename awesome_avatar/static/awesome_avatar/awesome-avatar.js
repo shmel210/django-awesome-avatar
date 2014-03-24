adf344fc9b9eb59284b7cde90211d2e3512ae8a2
Антон Белущенко <dilar05@i.ua>
2014-03-24 10:54:30 +0200

    bugfix

--- awesome_avatar/static/awesome_avatar/awesome-avatar.js
+++ awesome_avatar/static/awesome_avatar/awesome-avatar.js
@@ -1,25 +1,25 @@
-(function( $ ) {
+var j = jQuery.noConflict();
     function bind_on_change_input_file(selector, config) {
 
-        $(selector).change(function() {
-            if($(this).val() == '') {
+        j(selector).change(function() {
+            if(j(this).val() == '') {
                 return;
             }
 
             // if selection, remove
-            var $last_img = $(selector + '-select-area img');
-            if ($last_img.length) {
-                var img_obj = $last_img.imgAreaSelect({instance: true});
+            var jlast_img = j(selector + '-select-area img');
+            if (jlast_img.length) {
+                var img_obj = jlast_img.imgAreaSelect({instance: true});
                 img_obj.remove();
             }
 
-            $(selector + '-select-area').empty();
-    //        $(selector + '-select-area img').hide();
-    //        $(selector + '-preview').empty();
-            $(selector + '-preview img').hide();
+            j(selector + '-select-area').empty();
+    //        j(selector + '-select-area img').hide();
+    //        j(selector + '-preview').empty();
+            j(selector + '-preview img').hide();
 
-            var p = new RegExp(/\.(jpg|jpeg|png|gif)$/);
-            var fileanme = $(this).val().toLowerCase().replace(/^\s+|\s+$/g, '');
+            var p = new RegExp(/\.(jpg|jpeg|png|gif)j/);
+            var fileanme = j(this).val().toLowerCase().replace(/^\s+|\s+j/g, '');
             if(!p.test(fileanme)){
                 alert('{% trans "Неверный формат. Выберите изображение" %}');
                 return ;
@@ -37,42 +37,42 @@
 
     function bind_preview(selector, config, e) {
         var image_data = e.target.result;
-    //    $(selector + '-preview').empty();
-    //    $(selector + '-preview').append('<img />');
-        $(selector + '-preview img').show();
-        $(selector + '-preview img').css('max-width', 'none');
-        $(selector + '-preview img').attr('src', image_data);
+    //    j(selector + '-preview').empty();
+    //    j(selector + '-preview').append('<img />');
+        j(selector + '-preview img').show();
+        j(selector + '-preview img').css('max-width', 'none');
+        j(selector + '-preview img').attr('src', image_data);
 
-        $(selector + '-select-area').empty();
-        $(selector + '-select-area').append('<img />');
-    //    $(selector + '-select-area img').show();
-        $(selector + '-select-area img').attr('src', image_data).load(function(){
-            $(this).unbind('load');
+        j(selector + '-select-area').empty();
+        j(selector + '-select-area').append('<img />');
+    //    j(selector + '-select-area img').show();
+        j(selector + '-select-area img').attr('src', image_data).load(function(){
+            j(this).unbind('load');
 
-            var img_width = $(this).width();
-            var img_height = $(this).height();
+            var img_width = j(this).width();
+            var img_height = j(this).height();
 
             var ratio_x = img_width / config.select_area_width;
             var ratio_y = img_height / config.select_area_height;
 
             if (ratio_x > 1 || ratio_y > 1) {
                 if (ratio_x > ratio_y) {
-                    $(this).css('width', config.select_area_width + 'px');
-                    $(selector + '-ratio').val(ratio_x);
+                    j(this).css('width', config.select_area_width + 'px');
+                    j(selector + '-ratio').val(ratio_x);
                 }
                 else {
-                    $(this).css('height', config.select_area_height + 'px');
-                    $(selector + '-ratio').val(ratio_y);
+                    j(this).css('height', config.select_area_height + 'px');
+                    j(selector + '-ratio').val(ratio_y);
                 }
             } else {
-                $(selector + '-ratio').val(1);
+                j(selector + '-ratio').val(1);
             }
 
-            img_width = $(this).width();
-            img_height = $(this).height();
+            img_width = j(this).width();
+            img_height = j(this).height();
 
-            $(selector + '-width').val(ratio_x);
-            $(selector + '-height').val(ratio_y);
+            j(selector + '-width').val(ratio_x);
+            j(selector + '-height').val(ratio_y);
 
             var sel = {};
             sel['x1'] = Math.round(img_width/2-25 > 0 ? img_width/2-25 : 0),
@@ -81,7 +81,7 @@
             sel['y2'] = Math.round(img_height/2+25 > img_height ? img_height : img_height/2+25),
             sel['width'] = 50;
 
-            $(this).imgAreaSelect({
+            j(this).imgAreaSelect({
     //            handles: true,
                 aspectRatio: config.width + ":" + config.height,
                 fadeSpeed: 100,
@@ -99,16 +99,16 @@
     }
 
     function update_coors(selector, config, img, selection) {
-        $(selector + "-x1").val(selection.x1);
-        $(selector + "-y1").val(selection.y1);
-        $(selector + "-x2").val(selection.x2);
-        $(selector + "-y2").val(selection.y2);
+        j(selector + "-x1").val(selection.x1);
+        j(selector + "-y1").val(selection.y1);
+        j(selector + "-x2").val(selection.x2);
+        j(selector + "-y2").val(selection.y2);
 
         if (parseInt(selection.width) > 0) {
             var ratiox = config.width / (selection.width || 1);
             var ratioy = config.height / (selection.height || 1);
 
-            $(selector + '-preview img').css({
+            j(selector + '-preview img').css({
                 width: Math.round(ratiox * img.width) + 'px',
                 height: Math.round(ratioy * img.height) + 'px',
                 marginLeft: '-' + Math.round(ratiox * selection.x1) + 'px',
@@ -116,5 +116,4 @@
             });
         }
     }
-})(jQuery);
 
